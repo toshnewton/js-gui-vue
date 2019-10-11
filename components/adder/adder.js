aeeef5b19bc19b52f67d8685c0d78479464bfb7f
toshnewton <36524417+toshnewton@users.noreply.github.com>
2019-10-11 11:45:06 -0500

    changed labels

--- components/adder/adder.js
+++ components/adder/adder.js
@@ -3,11 +3,11 @@ const area = (x) => { return ((x * x) * Math.PI)}
 const adder = new Vue({
   el: '#adder',
   data: {
-    firstNumber: 5
+    radius: 5
   },
   computed: {
     result: function () {
-      const i = parseInt(this.firstNumber)
+      const i = parseInt(this.radius)
       return `Area is ${area(i)}.`
     }
   }
